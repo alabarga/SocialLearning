81574192588719d6920e858b68f799fe38490c2d
Yrth <laztnog@hotmail.com>
2014-11-05 14:24:35 +0100

    Primer paso de expand

--- SocialLearning/apps/learningobjects/management/commands/expand.py
+++ SocialLearning/apps/learningobjects/management/commands/expand.py
@@ -3,7 +3,24 @@ from learningobjects.utils.search import *
 from django.core.management.base import BaseCommand, CommandError
 from learningobjects.models import *
 from optparse import make_option
-
+import tweepy
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+import urllib2
+import hashlib
+import json
+import re
+from urlunshort import resolve
+#####TWITTER KEYS#####
+CONSUMER_KEY = 'ieZUZgZrSJJE0QLBBOsgXg'
+CONSUMER_SECRET = 'PlIpSrh6unKYZISSDieBIFAB3D9f6aSh4p4Dmcn8Q'
+OAUTH_TOKEN = '1015949947-0Akq5OBnEzTp7OwaIuvLNiKN6L52FNLVOW9yIyf'
+OAUTH_TOKEN_SECRET = 'SJz3nXcyGt2lIKhmPiFg5VlTdHLbrRSPRRgUZ552xfe1e'
+####Twitter auth handler####
+auth = OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
+twittApi = tweepy.API(auth)
 
 class Command(BaseCommand):
     args = 'query'
@@ -24,14 +41,50 @@ class Command(BaseCommand):
                 resources=Resource.objects.filter(status=Resource.DISCOVERED)
                 for res in resources:
                     url=res.url
-                    res.status=Resource.DISCOVERED
+                    #res.status=Resource.DISCOVERED
                     res.save()
                     print "Updated.."
         else:
             url=options['URL']
-            resource=Resource.objects.filter(url=url,status=Resource.DISCOVERED)
-            if len(resource)>0:
-                
-                resource.update(status=Resource.EXPANDED)
-            else:
-                print "That link is not in the database or is not with ´Discovered´ status. Add it first (python manage.py add -u "+url+")"
+            try:
+                resource=Resource.objects.get(url=url,status=Resource.DISCOVERED)
+            except:
+                print "That link is not in the database or is not with ´Described´ status. Add it first (python manage.py add -u "+url+")"
+            tags=resource.tags.all()
+            for tag in tags:
+                user="GontzalYrth"#getuser
+                get_expand(url,user,tag,"twitter")
+            #resource.update(status=Resource.EXPANDED)
+
+def get_expand(url,user,tag,social_network):
+    tagl=[]
+    tagl.append(str(tag))
+    relatedToTweet=[]        
+    if social_network=="twitter":
+        response=twittApi.user_timeline(user=user,count=10)
+        for tweet in response:
+            ht=extract_hash_tags(tweet.text)           
+            intersect=list(set(tagl) & set(ht))
+            if len(intersect)>0:
+                #relatedToTweet.append(tweet)
+                ##mirar si en el texto hay enlaces
+                ##para cada enlace dle texto
+                links= extract_urls(str(tweet.text))
+                for link in links:
+                    link=resolve(link)
+                    if link!=url:
+                        print link
+                """for link in links:
+                    Resource=self.createResource(link,firsturl,1)
+                    try:
+                        resources.insert(Resource)
+                        print "Succesfully inserted related resource twitter"
+                    except:
+                        print "Something went wrong you silly boy in related twitter"""
+
+def extract_hash_tags(s):
+    return set(part[1:] for part in s.split() if part.startswith('#'))
+
+def extract_urls(s):
+    a= re.findall('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+', s)
+    return a
