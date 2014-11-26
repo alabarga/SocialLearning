21d8c7c661b826a5d1116c25cdbfaf33c3226015
Alberto Labarga <alberto.labarga@gmail.com>
2014-11-26 07:05:46 +0100

    Remove duplicates

---
+++ SocialLearning/apps/learningobjects/management/commands/duplicates.py
@@ -0,0 +1,71 @@
+#-*- coding: UTF-8 -*-
+from learningobjects.utils.search import *
+from django.core.management.base import BaseCommand, CommandError
+from learningobjects.models import *
+from optparse import make_option
+from learningobjects.utils import feedfinder
+from learningobjects.management.commands import add
+from django.core.management import call_command
+import feedparser
+import hashlib
+import random
+import requests
+
+from learningobjects.utils.parsers import *
+
+class Command(BaseCommand):
+
+    help = 'Describe los documentos'
+
+    option_list = BaseCommand.option_list + (
+        make_option('--url',
+            dest='URL',
+            default=False,
+            help='URL'),
+        make_option('--log',
+            action='store_true',
+            dest='log',
+            default=False,
+            help='log info'),
+        )
+
+    def add_arguments(self, parser):
+        # Positional arguments
+        parser.add_argument('-u','--url',
+            nargs=1,
+            dest='URL',
+            help='URL del recurso')
+
+        #parser.add_argument('poll_id', nargs='+', type=int)
+
+        # Named (optional) arguments
+        parser.add_argument('--log',
+            nargs=0,
+            action='store_true',
+            dest='log',
+            default=False,
+            help='Log actions to console')
+
+
+    def handle(self, *args, **options):
+
+        resources=Resource.objects.all().update(identifier='')
+
+        resources=Resource.objects.all()
+        for res in resources:
+            try:
+
+                url = res.url
+                identifier = hashlib.md5(url).hexdigest()
+
+                try:
+                    resource=Resource.objects.get(identifier=identifier)
+                    print "%s duplicated!" % res.url
+                    res.delete()
+                except:
+                    res.identifier = identifier
+                    res.save()                
+
+            except:
+                continue
+
---
+++ SocialLearning/apps/learningobjects/utils/duplicates.py
@@ -0,0 +1,83 @@
+import unfurl
+import urllib2
+import json
+import tweepy
+from tweepy import Stream
+from tweepy import OAuthHandler
+from tweepy.streaming import StreamListener
+from learningobjects.utils.parsers import *
+
+from learningobjects.utils.unfurl import expand_url
+from urlunshort import resolve
+
+#####TWITTER KEYS#####
+CONSUMER_KEY = 'ieZUZgZrSJJE0QLBBOsgXg'
+CONSUMER_SECRET = 'PlIpSrh6unKYZISSDieBIFAB3D9f6aSh4p4Dmcn8Q'
+OAUTH_TOKEN = '1015949947-0Akq5OBnEzTp7OwaIuvLNiKN6L52FNLVOW9yIyf'
+OAUTH_TOKEN_SECRET = 'SJz3nXcyGt2lIKhmPiFg5VlTdHLbrRSPRRgUZ552xfe1e'
+####Twitter auth handler####
+auth = OAuthHandler(CONSUMER_KEY,CONSUMER_SECRET)
+auth.set_access_token(OAUTH_TOKEN,OAUTH_TOKEN_SECRET)
+twittApi = tweepy.API(auth)
+
+# { u'StumbleUpon': 0, 
+#   u'Reddit': 0, 
+#   u'GooglePlusOne': 0, 
+#   u'Pinterest': 0, 
+#   u'Twitter': 0, 
+#   u'Diggs': 0, 
+#   u'LinkedIn': 0, 
+#   u'Facebook': { u'commentsbox_count': 0, 
+#                  u'click_count': 0, 
+#                  u'total_count': 0, 
+#                  u'comment_count': 0, 
+#                  u'like_count': 0, 
+#                  u'share_count': 0 }, 
+#  u'Delicious': 0, 
+#  u'Buzz': 0
+# }
+
+def dictsum(d):
+    suma = 0
+    for k, v in d.iteritems():
+        if isinstance(v, dict):
+            suma += dictsum(v)
+        else:
+            suma += v
+
+class Interest(object):
+
+    def clean(self, url):
+
+        try:
+            furl = expand_url(url)
+        except:
+            furl = url
+
+        return furl
+
+    def __init__(self, url):
+
+        self.url = self.clean(url)
+
+        self.factores = {'LinkedIn': 2, 'Twitter': 2, 'Delicious':2 } 
+        self.suma = sum(self.factores.values())
+        for key in self.factores.keys():
+            self.factores[key] = self.factores[key] / self.suma        
+
+    def get_interest_count(self):
+
+        response=urllib2.urlopen('http://free.sharedcount.com/?url='+self.url+'&apikey=cabec5c5d636b063cbbcf8cbe966fd3c4c7d9152')
+        res=json.loads(response.read())
+        return res
+
+    def get_interest(self):
+
+        res = self.get_interest_count()
+
+        url_interest = res['Twitter'] + res['Facebook']['total_count']
+       
+        #for key in self.factores.keys():
+        #    url_interest += data[key] * self.factores[key] / suma
+
+        return url_interest 
