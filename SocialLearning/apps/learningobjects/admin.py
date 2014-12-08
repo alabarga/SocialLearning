23164478a693c4b726b557492e075e151bf66793
Alberto Labarga <alberto.labarga@gmail.com>
2014-12-09 00:04:06 +0100

    Admin

--- SocialLearning/apps/learningobjects/admin.py
+++ SocialLearning/apps/learningobjects/admin.py
@@ -13,10 +13,11 @@ class CollectionAdmin(admin.ModelAdmin):
 
 class ResourceAdmin(admin.ModelAdmin):
     list_display = ("identifier","title")
+    list_filter = ("status")
     search_fields = ("identifier","title","description")
 
 admin.site.register(Collection, CollectionAdmin)
-admin.site.register(Resource)
+admin.site.register(Resource, ResourceAdmin)
 admin.site.register(ResourceContainer)
 admin.site.register(SocialNetwork)
 admin.site.register(Mention)
