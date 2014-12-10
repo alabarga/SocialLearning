c8ac6e053c45dd67fb6a824d846e2c38e1e0bfb7
Alberto Labarga <alberto.labarga@gmail.com>
2014-12-10 22:10:19 +0100

    RSS feed

--- SocialLearning/apps/api/renderers.py
+++ SocialLearning/apps/api/renderers.py
@@ -18,18 +18,43 @@ class RSSRenderer(renderers.BaseRenderer):
             return ''
         
         fg = FeedGenerator()
-        fg.id(data['url'])
-        fg.title(data['name'])
-        fg.author( {'name':'SocialLearning','email':'social@hontza.es'} )
-        fg.link( href=data['url'], rel='alternate' )
-        fg.logo('http://www.ovtt.org/sites/default/files/styles/ampliacion_noticia/public/logo_hontza_fondo-claro_opaco.png')
-        fg.subtitle(data['description'])
-        fg.link( href=data['url']+'?format=rss', rel='self' )
-        fg.language('en')
+        myclass = type(renderer_context['view']).__name__ 
+
+        print renderer_context['request']
+        print myclass
+
+        if myclass == 'CollectionViewSet':
+           
+            feed_url = data['url']
+            feed_title = data['name']
+            feed_description = data['description']
+            resources = data['resources']
+        elif myclass == 'ResourceViewSet':
+            feed_url = "http://social.honzta.es/api/resources/"
+            feed_title = "SocialLearning resources"
+            feed_description = "SocialLearning resources"
+            resources = data
+        else: 
+            feed_url = "http://social.honzta.es/api/resources/"
+            feed_title = "SocialLearning resources"
+            feed_description = "SocialLearning resources"
+            resources = []
         
-        for item in data['resources']:
+        fg.id(feed_url)
+        fg.title(feed_title)
+        fg.subtitle(feed_description)
+        fg.author( {'name':'SocialLearning','email':'social@hontza.es'} )
+        fg.link( href=feed_url, rel='alternate' )
+        fg.logo('http://www.ovtt.org/sites/default/files/styles/ampliacion_noticia/public/logo_hontza_fondo-claro_opaco.png')        
+        fg.link( href=feed_url+'?format=rss', rel='self' )
+        fg.language('en')
+
+        for item in resources:
             fe = fg.add_entry()
-            fe.id(item['url'])
-            fe.title(item['title'])
+            fe.id(item['url'] or 'http://social.honzta.es/api/resources/1/')
+            fe.title(item['title'] or 'title')
+            #fe.link(item['url'])
+            #fe.content(item['title'])
+            #fe.description(item['description'])
 
         return fg.rss_str(pretty=True)
